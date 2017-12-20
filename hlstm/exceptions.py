dede5d432eaf4b9eafc03dbbc4c3f1794fa56487
Stepan <kuznec64@ya.ru>
2017-12-20 13:20:39 +0300

    add eceptions.py

---
+++ hlstm/exceptions.py
@@ -0,0 +1,14 @@
+
+class VariableNotFoundException(Exception):
+	def __init__(self, variable, where, msg=''):
+		self.variable = variable
+		self.where = where
+		self.msg = msg
+
+	def __str__(self):
+		message = "variable %s not found in %s" % (self.variable, self.where)
+		if self.msg:
+			message +='\n'+self.msg
+
+
+
