31c093f762852ce4988d9f4d25b898e2006d4bd4
gottalottarock <kuznec64@ya.ru>
2018-03-24 15:00:29 +0300

    change __init__.py

--- hlstm/__init__.py
+++ hlstm/__init__.py
@@ -1,7 +1,7 @@
-## from .interface import HLSTMInterface
-# from .logger import Logger
-# from .hlstm_model import HLSTMModel
-# from .tree_lstm import BinaryTreeLSTM
-# from .tree_lstm_cell import BinaryTreeLSTMCell
-# from .tree_binarizer import TreeBinarizer
-# from .preprocessing import prepare_embedding, prepare_labels
+from .interface import HLSTMInterface
+ from .logger import Logger
+ from .hlstm_model import HLSTMModel
+ from .tree_lstm import BinaryTreeLSTM
+ from .tree_lstm_cell import BinaryTreeLSTMCell
+ from .tree_binarizer import TreeBinarizer
+ from .preprocessing import prepare_embedding, prepare_labels
